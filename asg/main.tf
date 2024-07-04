11cf91967bb9fcbe56d32f9afc7b7dd0281c98e8
moulayhafidelhafa <167933385+moulayhafidelhafa@users.noreply.github.com>
2024-07-04 00:24:03 -0500

    edit asg

--- asg/main.tf
+++ asg/main.tf
@@ -1,40 +1,24 @@
-resource "aws_autoscaling_group" "example" {
-  availability_zones = ["us-east-1a"]
-  desired_capacity   = 1
-  max_size           = 2
-  min_size           = 1
-
-  launch_template {
-    id      = aws_launch_template.example.id
-    version = aws_launch_template.example.latest_version
+data "aws_autoscaling_groups" "groups" {
+  filter {
+    name   = "tag:Team"
+    values = ["Pets"]
   }
-
-  tag {
-    key                 = "Key"
-    value               = "Value"
-    propagate_at_launch = true
-  }
-
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50
-    }
-    triggers = ["tag"]
-  }
-}
-
-data "aws_ami" "example" {
-  most_recent = true
-  owners      = ["amazon"]
 
   filter {
-    name   = "name"
-    values = ["ami-06c68f701d8090592"]
+    name   = "tag-key"
+    values = ["Environment"]
   }
 }
 
-resource "aws_launch_template" "example" {
-  image_id      = data.aws_ami.example.id
-  instance_type = "t3.nano"
+resource "aws_autoscaling_notification" "slack_notifications" {
+  group_names = data.aws_autoscaling_groups.groups.names
+
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+    "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
+  ]
+
+  topic_arn = "TOPIC ARN"
 }
