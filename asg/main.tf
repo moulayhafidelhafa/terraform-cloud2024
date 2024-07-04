e45b722181a65debb9a87c7289eb32002916286f
moulayhafidelhafa <167933385+moulayhafidelhafa@users.noreply.github.com>
2024-07-04 00:04:22 -0500

    added asg

---
+++ asg/main.tf
@@ -0,0 +1,40 @@
+resource "aws_autoscaling_group" "example" {
+  availability_zones = ["us-east-1a"]
+  desired_capacity   = 1
+  max_size           = 2
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = aws_launch_template.example.latest_version
+  }
+
+  tag {
+    key                 = "Key"
+    value               = "Value"
+    propagate_at_launch = true
+  }
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+  }
+}
+
+data "aws_ami" "example" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_launch_template" "example" {
+  image_id      = data.aws_ami.example.id
+  instance_type = "t3.nano"
+}
